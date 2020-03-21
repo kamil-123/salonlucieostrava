af4b2491c19e43033759e0d7dfb16b990a343321
Ayumi Osawa <ayumi@ayumis-macbook-air.home>
2020-03-21 12:25:40 +0100

    models and controllers initialized, routes/api.php set up

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// The following line contains: 
+//     - GET method to 'api/booking' using BookingController@index()
+//     - POST method to 'api/booking' using BookingController@store()
+//     - GET method to 'api/booking/create' using BookingController@create()
+//     - GET method to 'api/booking/{id}' using BookingController@show()
+//     - PUT method to 'api/booking/{id}' using BookingController@update()
+//     - DELETE method to 'api/booking/{id}' using BookingController@delete()
+//     - GET method to 'api/booking/{id}/edit' using BookingController@edit()
+Route::resource('booking', 'BookingController');
+
+
+//     - GET: 'api/customer': CustomerController@index()
+//     - POST: 'api/customer': CustomerController@store()
+//     - GET: 'api/customer/create': CustomerController@create()
+//     - GET: 'api/customer/{id}': CustomerController@show()
+//     - PUT: 'api/customer/{id}': CustomerController@update()
+//     - DELETE: 'api/customer/{id}': CustomerController@delete()
+//     - GET: 'api/customer/{id}/edit': CustomerController@edit()
+Route::resource('customer', 'CustomerController');
+
+
+//     - GET: 'api/stylist': StylistController@index()
+//     - POST: 'api/stylist': StylistController@store()
+//     - GET: 'api/stylist/create': StylistController@create()
+//     - GET: 'api/stylist/{id}': StylistController@show()
+//     - PUT: 'api/stylist/{id}': StylistController@update()
+//     - DELETE: 'api/stylist/{id}': StylistController@delete()
+//     - GET: 'api/stylist/{id}/edit': StylistController@edit()
+Route::resource('stylist', 'StylistController');
+
+
+//     - GET: 'api/treatment': TreatmentController@index()
+//     - POST: 'api/treatment': TreatmentController@store()
+//     - GET: 'api/treatment/create': TreatmentController@create()
+//     - GET: 'api/treatment/{id}': TreatmentController@show()
+//     - PUT: 'api/treatment/{id}': TreatmentController@update()
+//     - DELETE: 'api/treatment/{id}': TreatmentController@delete()
+//     - GET: 'api/treatment/{id}/edit': TreatmentController@edit()
+Route::resource('treatment', 'TreatmentController');
