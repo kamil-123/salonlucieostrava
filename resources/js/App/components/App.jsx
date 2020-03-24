edf2196d2221efb49144ad30903a2b4417af705c
Kamil Drozd <drozdk@centrum.cz>
2020-03-24 17:14:49 +0100

    react includes routes, web.php is updated

--- resources/js/App/components/App.jsx
+++ resources/js/App/components/App.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import Main from './Main/Main.jsx';
+import Stylistselect from './Stylistselect/Stylistselect.jsx';
 
  
 
@@ -7,8 +14,15 @@ export default class App extends React.Component {
     render() {
 
         return (
-
-            <h1>App component</h1>
+            <>
+            <Router>
+                <Switch>
+                    <Route path='/customers/stylistselect' component={Stylistselect}/>    
+                    <Route path='/customers' component={Main}/>
+                    
+                </Switch>
+            </Router>
+            </>
 
  
 
