80243013db02522dd630e2834632eaf9059eb8b9
Ayumi Osawa <ayumi@Ayumis-MacBook-Air.local>
2020-04-01 14:38:14 +0200

    stylist side, calendar view, links to the details added(functional)

--- resources/views/stylist/calendar.blade.php
+++ resources/views/stylist/calendar.blade.php
@@ -49,94 +49,17 @@
   [data-toggle="calendar"] > .row:last-child > .calendar-day {
     border-bottom-width: 1px;
   }
-  	/* .popover.calendar-event-popover {
-    font-family: 'Roboto', sans-serif;
-    font-size: 12px;
-    color: rgb(120, 120, 120);
-    border-radius: 2px;
-    max-width: 300px;
+  .event > .event {
+    display: flex;
+    flex-flow: column nowrap;
+    overflow: hidden;
   }
-  .popover.calendar-event-popover h4 {
-    font-size: 14px;
-    font-weight: 900;
-  }
-  .popover.calendar-event-popover .location,
-  .popover.calendar-event-popover .datetime {
-    font-size: 14px;
-    font-weight: 700;
-    margin-bottom: 5px;
-  }
-  .popover.calendar-event-popover .location > span,
-  .popover.calendar-event-popover .datetime > span {
-    margin-right: 10px;
-  }
-  .popover.calendar-event-popover .space,
-  .popover.calendar-event-popover .attending {
-    margin-top: 10px;
-    padding-bottom: 5px;
-    border-bottom: 1px solid rgb(160, 160, 160);
-    font-weight: 700;
-  }
-  .popover.calendar-event-popover .space > .pull-right,
-  .popover.calendar-event-popover .attending > .pull-right {
-    font-weight: 400;
-  }
-  .popover.calendar-event-popover .attending {
-    margin-top: 5px;
-    font-size: 18px;
-    padding: 0px 10px 5px;
-  }
-  .popover.calendar-event-popover .attending img {
-    border-radius: 50%;
-    width: 40px;
-  }
-  .popover.calendar-event-popover .attending span.attending-overflow {
-    display: inline-block;
-    width: 40px;
-    background-color: rgb(200, 200, 200);
-    border-radius: 50%;
-    padding: 8px 0px 7px;
-    text-align: center;
-  }
-  .popover.calendar-event-popover .attending > .pull-right {
-    font-size: 28px;
-  }
-  .popover.calendar-event-popover a.btn {
-    margin-top: 10px;
-    width: 100%;
-    border-radius: 3px;
-  } 
-  .calendar-day > time {
-  position: absolute;
-  display: block;
-  bottom: 0px;
-  left: 0px;
-  font-size: 12px;
-  font-weight: 300;
-  width: 100%;
-  padding: 10px 10px 3px 0px;
-  text-align: right;
-  }
-  .calendar-day > .events {
-  cursor: pointer;
-  }
-  .calendar-day > .events > .event h4 {
+  .events > .event > a {
   font-size: 12px;
   font-weight: 700;
   white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
   margin-bottom: 3px;
   }
-  .calendar-day > .events > .event > .desc,
-  .calendar-day > .events > .event > .location,
-  .calendar-day > .events > .event > .datetime,
-  .calendar-day > .events > .event > .attending {
-  display: none;
-  }
-  .calendar-day > .events > .event > .progress {
-  height: 10px;
-  } */
 </style>
 @endsection
 
@@ -188,35 +111,33 @@
                           </div>
                         </div>
                         
-                        @foreach($date_list as $day)
-                            @if( $day['weekday'] === '1' ) {{-- Monday--}}
-                              <div class="row">
-                                @if( $day['month'] !== date('m') )
-                                  <div class='col calendar-day calendar-no-current-month'>
-                                @else
-                                  <div class='col calendar-day'>
-                                @endif
-                                    <time datetime="{{ $day['full'] }}">{{ $day['day'] }}</time>
-                                  </div>
-                            @elseif( $day['weekday'] === '0' ) {{-- Sunday --}}
-                                @if( $day['month'] !== date('m') )
-                                  <div class='col calendar-day calendar-no-current-month'>
-                                @else
-                                  <div class='col calendar-day'>
-                                @endif
-                                  <time datetime="{{ $day['full'] }}">{{ $day['day'] }}</time>
-                                </div>
-                              </div>
-                            @else    {{-- Other Days --}}
-                              @if( $day['month'] !== date('m') )
-                                <div class='col calendar-day calendar-no-current-month'>
-                              @else
-                                <div class='col calendar-day'>
-                              @endif
-                                  <time datetime="{{ $day['full'] }}">{{ $day['day'] }}</time>
-                                </div>
-                            @endif
+                        @foreach($date_list as $index => $day)
 
+                          {{-- Open a new week(row) if Monday  --}}
+                          @if( $day['weekday'] === '1' ) {{-- Monday --}}
+                            <div class="row">
+                          @endif
+                            @if( $day['month'] !== date('m') )
+                              <div class='col calendar-day calendar-no-current-month'>
+                            @else
+                              <div class='col calendar-day'>
+                            @endif
+                                <time datetime="{{ $day['full'] }}">{{ $day['day'] }}</time>
+                                  {{-- add bookings if they exist --}}
+                                  @foreach( $bookings as $booking )
+                                    @if ( $booking['date'] === $day['formatted'] && $booking['availability']=== 1) 
+                                      <div class="events">
+                                        <div class="event">
+                                          <a href={{ route('booking.details', ['id' => $booking->id ])}}>{{ $booking['time'] }}</a>
+                                        </div>
+                                      </div>
+                                    @endif
+                                  @endforeach
+                              </div>
+                          {{-- Close the week(row) if Sunday --}}
+                          @if( $day['weekday'] === '0' )
+                            </div>
+                          @endif
                         @endforeach
                         
                 </div>
